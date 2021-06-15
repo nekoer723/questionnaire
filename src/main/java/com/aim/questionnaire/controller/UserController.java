562769b6aa754eb992834153a20265bf49b05b45
nekoer <wxj1151737383@163.com>
2021-06-15 19:01:35 +0800

    人脸识别

--- src/main/java/com/aim/questionnaire/controller/UserController.java
+++ src/main/java/com/aim/questionnaire/controller/UserController.java
@@ -2,9 +2,13 @@ package com.aim.questionnaire.controller;
 
 import com.aim.questionnaire.beans.HttpResponseEntity;
 import com.aim.questionnaire.common.Constans;
+import com.aim.questionnaire.common.utils.Base64Util;
 import com.aim.questionnaire.common.utils.ExcelUtil;
+import com.aim.questionnaire.common.utils.GsonUtils;
+import com.aim.questionnaire.common.utils.HttpUtil;
 import com.aim.questionnaire.dao.entity.UserEntity;
 import com.aim.questionnaire.service.UserService;
+import com.alibaba.fastjson.JSONObject;
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -39,6 +43,70 @@ public class UserController {
             httpResponseEntity.setData(hasUser);
             httpResponseEntity.setCode(Constans.SUCCESS_CODE);
             httpResponseEntity.setMessage("登陆失败");
+        }
+        return httpResponseEntity;
+    }
+
+    @RequestMapping(value = "/addFace",method = RequestMethod.POST)
+    public void addFace(@RequestBody Map<String,Object> maps){
+        String url = "https://aip.baidubce.com/rest/2.0/face/v3/faceset/user/add";
+        try {
+            Map<String, Object> map = new HashMap<>();
+            map.put("image", maps.get("image"));
+            map.put("group_id", "group_repeat");
+            map.put("user_id", "user1");
+            map.put("user_info", "abc");
+            map.put("liveness_control", "NORMAL");
+            map.put("image_type", "BASE64");
+            map.put("quality_control", "LOW");
+
+            String param = GsonUtils.toJson(map);
+
+            // 注意这里仅为了简化编码每一次请求都去获取access_token，线上环境access_token有过期时间， 客户端可自行缓存，过期后重新获取。
+            String accessToken = "24.3792f22475dbfa977fbae4315e277eec.2592000.1626333769.282335-24371651";
+
+            String result = HttpUtil.post(url, accessToken, "application/json", param);
+            System.out.println(result);
+            //return result;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        //return null;
+    }
+
+
+    @RequestMapping(value = "/loginFace",method = RequestMethod.POST)
+    public HttpResponseEntity loginFace(@RequestBody Map<String,Object> maps){
+        HttpResponseEntity httpResponseEntity = new HttpResponseEntity();
+        UserEntity hasUser = userService.selectAllByName("admin");
+        String url = "https://aip.baidubce.com/rest/2.0/face/v3/match";
+        Map<String,Object> pic1 = new HashMap<>();
+        pic1.put("image", Base64Util.ImageToBase64("src/main/resources/static/images/me.png"));
+        pic1.put("image_type","BASE64");
+        List<Map<String,Object>> map = new ArrayList<>();
+        map.add(maps);
+        map.add(pic1);
+        try {
+
+            String param = GsonUtils.toJson(map);
+            System.out.println(param);
+
+            // 注意这里仅为了简化编码每一次请求都去获取access_token，线上环境access_token有过期时间， 客户端可自行缓存，过期后重新获取。
+            String accessToken = "24.3792f22475dbfa977fbae4315e277eec.2592000.1626333769.282335-24371651";
+
+            String result = HttpUtil.post(url, accessToken, "application/json", param);
+            System.out.println(result);
+            //return result;
+            JSONObject jsonObject = JSONObject.parseObject(result);
+            double score = (jsonObject.getJSONObject("result").getDouble("score"));
+            //System.out.println(score+"dsddsfffffffffffffff________________________________________");
+            if(score>90){
+                httpResponseEntity.setCode(Constans.SUCCESS_CODE);
+                httpResponseEntity.setData(hasUser);
+                httpResponseEntity.setMessage("登录成功");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
         }
         return httpResponseEntity;
     }
--- src/main/resources/static/pages/answerQuestionJs/login.js
+++ src/main/resources/static/pages/answerQuestionJs/login.js
@@ -5,6 +5,78 @@ var UserNameText = '';
 
 // isLoginFun();
 //登录
+function picture(){
+    var context = document.getElementById("canvas").getContext("2d");
+    context.drawImage(video, 0, 0, 180, 180);
+}
+function loginFace(){
+    let video = document.getElementById("video");
+    video.style.display = "block";
+    let canvas = document.getElementById("canvas");
+    let context = canvas.getContext("2d");
+    // 老的浏览器可能根本没有实现 mediaDevices，所以我们可以先设置一个空的对象
+    if (navigator.mediaDevices === undefined) {
+        navigator.mediaDevices = {};
+    }
+    // 一些浏览器部分支持 mediaDevices。我们不能直接给对象设置 getUserMedia
+    // 因为这样可能会覆盖已有的属性。这里我们只会在没有getUserMedia属性的时候添加它。
+    if (navigator.mediaDevices.getUserMedia === undefined) {
+        navigator.mediaDevices.getUserMedia = function (constraints) {
+            // 首先，如果有getUserMedia的话，就获得它
+            var getUserMedia = navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
+
+            // 一些浏览器根本没实现它 - 那么就返回一个error到promise的reject来保持一个统一的接口
+            if (!getUserMedia) {
+                return Promise.reject(new Error('getUserMedia is not implemented in this browser'));
+            }
+            // 否则，为老的navigator.getUserMedia方法包裹一个Promise
+            return new Promise(function (resolve, reject) {
+                getUserMedia.call(navigator, constraints, resolve, reject);
+            });
+        }
+    }
+    //默认使用前摄像头，强制使用后置摄像头如下设置
+    // let constraints = {video: { facingMode: { exact: "environment" } }};
+    let constraints = {video: true};
+    navigator.mediaDevices.getUserMedia(constraints)
+        .then(function (stream) {
+            // 旧的浏览器可能没有srcObject
+            if ("srcObject" in video) {
+                video.srcObject = stream;
+            } else {
+                // 防止在新的浏览器里使用它，应为它已经不再支持了
+                video.src = window.URL.createObjectURL(stream);
+            }
+            video.onloadedmetadata = function (e) {
+                video.play();
+                document.getElementById("capture").style.display = "block";
+                document.getElementById("stop").style.display = "block";
+            };
+        })
+        .catch(function (err) {
+            console.log(err.name + ": " + err.message);
+        });
+    document.getElementById("capture").addEventListener("click", function () {
+        //绘制画面
+        context.drawImage(video, 0, 0, 180, 180);// 保存base64
+        var imgData = document.getElementById("canvas").toDataURL("image/png");
+        var base64 = imgData.substr(22);
+        var data = {
+            image : base64,
+            image_type:"BASE64"
+        }
+        console.log(data);
+        commonAjaxPost(true,"/admin/loginFace",data,loginSuccess);
+    });
+    document.getElementById("stop").addEventListener("click",function () {
+        document.getElementById("capture").style.display = "none";
+        document.getElementById("stop").style.display = "none";
+        video.style.display = "none";
+        this.mediaStreamTrack.getTracks()[0].stop();
+        this.mediaStreamTrack.getTracks()[1].stop();
+    })
+}
+
 function login() {
     if (!UserName.value) {
         alert("请先输入用户名");
@@ -29,11 +101,11 @@ function login() {
 
 //登录成功回调
 function loginSuccess(result){
-    if (result.code == '666') {
+    if (result.code === '666') {
         layer.msg(result.message, {icon:1});
         setCookie('isLogin','1');
         setCookie('userId',result.data.id);
-        setCookie('userName',UserNameText);
+        setCookie('userName',result.data.username);
         setCookie('power',result.data.role);
         setCookie('modelId',result.data.modelId)
         window.location.href = "myQuestionnaires.html"
